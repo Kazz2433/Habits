cad9817d0cdab86aaeeb2ff9656bfd936c1ffb1c
Kazz2433 <kelvinquida24@gmail.com>
2023-01-24 00:26:01 -0400

    added fillCalendarDays

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,6 @@
+import generateDatesFromYearBeginning from "../utils/generate-dates-from-year-beginning"
+import { HabitDay } from "./HabitDay"
+
 const weekDays = [
     'S',
     'M',
@@ -7,6 +10,11 @@ const weekDays = [
     'F',
     'S',
 ]
+
+const summaryDates = generateDatesFromYearBeginning()
+
+const minimumSummaryDatesSize = 18 * 7 //18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 export default function SummaryTable() {
   return (
@@ -25,12 +33,18 @@ export default function SummaryTable() {
         </div>
 
         <div className="grid grid-rows-7 grid-flow-col gap-3 ">
-            <div className="w-10 h-10 bg-zinc-900 border-2
-             border-zinc-800 rounded-lg">
-            
-            </div>
-            <div className="w-10 h-10 bg-zinc-900 border-2
-             border-zinc-800 rounded-lg"></div>
+            {summaryDates.map(date =>{
+                return <HabitDay key={date.toString()}/>
+            })}
+
+            {amountOfDaysToFill > 0 && Array.from({length:amountOfDaysToFill}).map((_,i) =>{
+                return (
+                    <div
+                        key={i} 
+                        className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+                    />
+                )
+            })}
         </div>
     </div>
   )
